fb88296e8ad7d4eb4c27c91a338b44ddb3b83222
Kouhei Maeda <mkouhei@palmtb.net>
2014-09-02 07:38:45 +0900

    Added skelton Django App.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ nenga/core/urls.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('', url(r'^admin/', include(admin.site.urls)),)
---
+++ setup.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2014 Kouhei Maeda <mkouhei@palmtb.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: "
+    "GNU General Public License v3 or later (GPLv3+)",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Internet",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+]
+
+
+long_description = (
+    open("README.rst").read())
+
+requires = ['setuptools',
+            'Django',
+            'django_shortuuidfield',
+            'genenga']
+
+setup(name='nenga',
+      version='0.1.0',
+      description='Web UI to generate PDF of Japanese Nengajo',
+      long_description=long_description,
+      author='Kouhei Maeda',
+      author_email='mkouhei@palmtb.net',
+      url='https://github.com/mkouhei/nenga',
+      license='GNU General Public License version 3',
+      classifiers=classifiers,
+      packages=['nenga'],
+      data_files=[],
+      install_requires=requires,
+      include_package_data=True,
+      tests_require=['tox'],
+      cmdclass={'test': Tox},)
