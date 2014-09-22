76b6460afc96ef6e11781b4392c56ba7d3700036
Kouhei Maeda <mkouhei@palmtb.net>
2014-09-22 22:30:21 +0900

    Append generate_data.py, and execute generate_data at testenv commands.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ nenga_tests/test_data/generate_data.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import shortuuid
+from datetime import datetime
+
+
+def generate_data(key):
+    return {
+        "model": "nenga.%s" % key,
+        "pk": shortuuid.uuid(),
+        "fields": {
+            "%s" % key: shortuuid.uuid()
+        }
+    }
+
+
+def generate_contact(owner_id):
+    return {
+        "model": "address.contact",
+        "pk": shortuuid.uuid(),
+        "fields": {
+            "last_name": shortuuid.uuid(),
+            "first_name": shortuuid.uuid(),
+            "zip_code": shortuuid.uuid(),
+            "prefecture": shortuuid.uuid(),
+            "city": shortuuid.uuid(),
+            "address": shortuuid.uuid(),
+            "address2": shortuuid.uuid(),
+            "partner_name": shortuuid.uuid(),
+            "owner": owner_id
+        }
+    }
+
+
+def generate_json():
+    data_list = []
+    for i in range(20):
+        data_list.append(generate_contact(i % 4 + 1))
+    return json.dumps(data_list)
+
+
+def main():
+    dirpath = os.path.dirname(os.path.abspath(__file__))
+    json_path = os.path.join(dirpath, 'dummy_data.json')
+
+    with open(json_path, 'w') as f:
+        f.write(generate_json())
+
+
+if __name__ == "__main__":
+    main()
