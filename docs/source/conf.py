fe077b51df248b9536962163500c955b3f375e72
Kouhei Maeda <mkouhei@palmtb.net>
2015-09-25 05:24:22 +0900

    Fixes the short version of Sphinx documentation.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ copyright = u'2014, Kouhei Maeda'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.0'
 
